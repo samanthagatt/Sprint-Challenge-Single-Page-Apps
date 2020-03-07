e4e4268ee6c839e45d8e8b17d9a7a4325807a971
Samantha Gatt <smfgatt@gmail.com>
2020-03-07 11:46:35 -0500

    Sets up main routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
-import Header from "./components/Header.js";
+import { Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import WelcomePage from "./components/WelcomePage";
+import SearchForm from "./components/SearchForm";
+import CharacterList from "./components/CharacterList";
 
 export default function App() {
   return (
     <main data-testid='app'>
       <Header />
+      <Switch>
+        <Route path="/search">
+          <SearchForm />
+          <CharacterList />
+        </Route>
+        <Route path="/">
+          <WelcomePage />
+        </Route>
+      </Switch>
     </main>
   );
 }
